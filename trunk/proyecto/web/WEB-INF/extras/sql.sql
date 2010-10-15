9899a950d940ad0626be1e58e16c481cb94df292
priospino@gmail.com <priospino@gmail.com@fcd89af8-110f-dd40-7fa0-076c3cdc3992>
2010-10-15 05:29:05 +0000

    DER, SCRIPT Y DER EN XML

---
+++ trunk/proyecto/web/WEB-INF/extras/sql.sql
@@ -0,0 +1,84 @@
+CREATE TABLE TB_Cartelera (
+  id_cartelera INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  id_Pelicula INTEGER UNSIGNED NOT NULL,
+  num_Sala INTEGER UNSIGNED NOT NULL,
+  fecha_inicio DATE NULL,
+  hora TIME NOT NULL,
+  fecha_fin DATE NULL,
+  PRIMARY KEY(id_cartelera),
+  INDEX TB_Cartelera_FKIndex1(num_Sala),
+  INDEX TB_Cartelera_FKIndex2(id_Pelicula)
+);
+
+CREATE TABLE TB_Cliente (
+  id_Cliente INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR NULL,
+  ape_paterno VARCHAR NULL,
+  ape_materno VARCHAR NULL,
+  dni VARCHAR NULL,
+  telefono VARCHAR NULL,
+  email VARCHAR NULL,
+  tipo_tarjeta INTEGER UNSIGNED NULL,
+  num_tarjeta VARCHAR NULL,
+  PRIMARY KEY(id_Cliente)
+);
+
+CREATE TABLE TB_Detalle_Venta (
+  id_Venta INTEGER UNSIGNED NOT NULL,
+  num_butaca INTEGER UNSIGNED NULL,
+  INDEX TB_Detalle_Venta_FKIndex1(id_Venta)
+);
+
+CREATE TABLE TB_Pelicula (
+  id_Pelicula INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR NULL,
+  genero VARCHAR(20) NULL,
+  duracion TIME NULL,
+  director VARCHAR(255) NULL,
+  actores TEXT NULL,
+  censura INTEGER UNSIGNED NULL,
+  web VARCHAR(255) NULL,
+  sinopsis VARCHAR NULL,
+  periodo INTEGER UNSIGNED NULL,
+  foto VARCHAR NULL,
+  PRIMARY KEY(id_Pelicula)
+);
+
+CREATE TABLE TB_Puntos (
+  id_Cliente INTEGER UNSIGNED NOT NULL,
+  cant_puntos INTEGER UNSIGNED NULL,
+  INDEX TB_Puntos_FKIndex1(id_Cliente)
+);
+
+CREATE TABLE TB_Sala (
+  num_Sala INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  capacidad INTEGER UNSIGNED NULL,
+  3d BOOL NULL,
+  PRIMARY KEY(num_Sala)
+);
+
+CREATE TABLE TB_Usuario (
+  id_Usuario INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  password_2 VARCHAR NULL,
+  nombre VARCHAR NULL,
+  ape_paterno VARCHAR NULL,
+  ape_materno VARCHAR NULL,
+  perfil VARCHAR NULL,
+  dni VARCHAR NULL,
+  PRIMARY KEY(id_Usuario)
+);
+
+CREATE TABLE TB_Venta (
+  id_Venta INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  id_cartelera INTEGER UNSIGNED NOT NULL,
+  id_Cliente INTEGER UNSIGNED NOT NULL,
+  id_Usuario INTEGER UNSIGNED NOT NULL,
+  fecha_venta DATETIME NULL,
+  cant_entradas INTEGER UNSIGNED NULL,
+  promocion BOOL NULL,
+  PRIMARY KEY(id_Venta),
+  INDEX TB_Venta_FKIndex1(id_Cliente),
+  INDEX TB_Venta_FKIndex3(id_Usuario)
+);
+
+
