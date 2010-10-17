4f5a41bae2ca26953c0b67ef439ee3014b2d493c
priospino@gmail.com <priospino@gmail.com@fcd89af8-110f-dd40-7fa0-076c3cdc3992>
2010-10-17 00:17:57 +0000

    mejoras en la estructura de paquetes

---
+++ trunk/proyecto/src/java/edu/upc/dew/cinepelis/model/Venta.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.upc.dew.cinepelis.model;
+
+/**
+ *
+ * @author Alyssa
+ */
+public class Venta {
+
+}
