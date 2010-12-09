f99969a52992e7ef8096740eca0aa77fc6a04197
just.johana <just.johana@fcd89af8-110f-dd40-7fa0-076c3cdc3992>
2010-12-09 01:35:05 +0000

    Lista de Clientes is done =)

--- trunk/proyecto/src/java/edu/upc/dew/cinepelis/controller/jsf/ClienteJSF.java
+++ trunk/proyecto/src/java/edu/upc/dew/cinepelis/controller/jsf/ClienteJSF.java
@@ -17,9 +17,20 @@ import java.util.List;
  */
 public class ClienteJSF extends  GenericBean{
 
-     List<ClienteBean> lstCliente;
+    List<ClienteBean> lstCliente;
+    private Long id_cliente;
+    private String nombre;
+    private String ape_paterno;
+    private String ape_materno;
+    private String dni;
+    private String telefono;
+    private String email;
+    private String tipo_tarjeta;
+    private String num_tarjeta;
 
-    public String initClientes(){
+
+
+     public String initClientes(){
         log.info("Entrando ... initClientes() - ClienteJSF");
 
         String forward="clientes";
@@ -45,7 +56,49 @@ public class ClienteJSF extends  GenericBean{
         return lstCliente;
     }
 
-    
 
+    public String getApe_materno() {
+        return ape_materno;
+    }
 
+    public String getApe_paterno() {
+        return ape_paterno;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Long getId_cliente() {
+        return id_cliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getNum_tarjeta() {
+        return num_tarjeta;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getTipo_tarjeta() {
+        return tipo_tarjeta;
+    }
+
+         public void resetForm(){
+         id_cliente=null;
+         nombre=null;
+         ape_paterno=null;
+         ape_materno=null;
+         dni=null;
+         telefono=null;
+         email=null;
+         tipo_tarjeta=null;
+         num_tarjeta=null;
+     }
+   
 }
--- trunk/proyecto/src/java/edu/upc/dew/cinepelis/dao/ibatisImpl/ClienteDAOMysql.java
+++ trunk/proyecto/src/java/edu/upc/dew/cinepelis/dao/ibatisImpl/ClienteDAOMysql.java
@@ -19,14 +19,22 @@ public class ClienteDAOMysql extends SqlMapClientDaoSupport implements ClienteDA
 
     public List<ClienteBean> getClientes() {
 
-       List<ClienteBean> lstCliente = new ArrayList<ClienteBean>();
+        try{
+            return (List<ClienteBean>)getSqlMapClientTemplate().queryForList("listarCliente", null);
+	 }catch (Exception e) {
+		e.printStackTrace();
+		System.out.println("Error en el ibatis - method getCliente()");
+		return null;
+	}
+
+      /* List<ClienteBean> lstCliente = new ArrayList<ClienteBean>();
        lstCliente.add(new ClienteBean(Long.valueOf(1), "Jazmin", "Sepulveda", "Reyes", "45553638", "4658875", "jazmin@hotmail.com", null, null));
        lstCliente.add(new ClienteBean(Long.valueOf(2), "Harold", "Pajuelo", "Cabrera", "25364455", "1256489", "rarol@hotmail.com", null, null));
        lstCliente.add(new ClienteBean(Long.valueOf(3), "Chiara", "Hidalgo", "Montesinos", "48796532", "5548961", "chiara@hotmail.com", null, null));
-       
-
-
        return lstCliente;
+        */
+
+       
     }
 
     public ClienteBean findCustomerByTarjeta(Long numTarjeta) {
